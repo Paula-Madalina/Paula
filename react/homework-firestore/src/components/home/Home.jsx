c6fa03992c286ef3ff89bd456d80740334f71bc2
Paula-Madalina <prepemadalinutza@yahoo.com>
2024-08-02 21:20:38 +0300

    dfg

--- react/homework-firestore/src/components/home/Home.jsx
+++ react/homework-firestore/src/components/home/Home.jsx
@@ -4,63 +4,37 @@ import { useNavigate, Link } from 'react-router-dom';
 import Header from '../header/Header';
 import UsersLogged from '../Users/UsersLogged';
 
-// function Home() {
-//   const navigate = useNavigate();
-//   const { currentUser } = useAuth();
-//   const [showUsers, setShowUsers] = useState(false);
-
-//   useEffect(() => {
-//     console.log(currentUser);
-//     if (!currentUser) {
-//       navigate('/login');
-//     }
-//   }, [currentUser, navigate]);
-
-//   return (
-//     <div>
-//       <Header />
-//       <div>Hello
-//         {
-//           currentUser ?
-//           currentUser.email :
-//           "Placeholder"
-//         }
-//       </div>
-//       <br />
-//       <div className='table__display'>
-//         <Link to="#" onClick={() => setShowUsers(true)}>View Users</Link> 
-//         <Link to="#" onClick={() => setShowUsers(false)}>Hide Users</Link>
-//       </div>
-//       {showUsers && <UsersLogged />}
-//     </div>
-//   );
-// }
-
-// export default Home;
-
-
 function Home() {
-  const {currentUser} = useAuth();
   const navigate = useNavigate();
-  const [showUsers, setShowUsers] = useState(false)
+  const { currentUser } = useAuth();
+  const [showUsers, setShowUsers] = useState(false);
 
-  useEffect(()=> {
-    if(!currentUser) {
-      navigate("/login")
+  useEffect(() => {
+    console.log(currentUser);
+    if (!currentUser) {
+      navigate('/login');
     }
-  },[currentUser, navigate])
+  }, [currentUser, navigate]);
 
-  return(
+  return (
     <div>
-    <Header/>
-    <div> Hello {currentUser ? currentUser.email : ""}</div>
-    <div>
-      <Link to="#" onClick={() => {setShowUsers(true)}}>Show Users</Link>
-      <Link to="#" onClick={() => {setShowUsers(false)}}>Hide Users</Link>
+      <Header />
+      <div>Hello
+        {
+          currentUser ?
+          currentUser.email :
+          "Placeholder"
+        }
+      </div>
+      <br />
+      <div className='table__display'>
+        <Link to="#" onClick={() => setShowUsers(true)}>View Users</Link> 
+        <Link to="#" onClick={() => setShowUsers(false)}>Hide Users</Link>
+      </div>
+      {showUsers && <UsersLogged />}
     </div>
-    {showUsers && <UsersLogged/>}
-    </div>
-  )
+  );
 }
 
 export default Home;
+
