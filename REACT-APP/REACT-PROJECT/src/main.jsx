f88d55e94903e7872be2763a0bdd5aa88150d657
Paula-Madalina <prepemadalinutza@yahoo.com>
2024-08-07 19:26:54 +0300

    dfggf

--- REACT-APP/REACT-PROJECT/src/main.jsx
+++ REACT-APP/REACT-PROJECT/src/main.jsx
@@ -2,9 +2,26 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import 'boxicons/css/boxicons.min.css';
+
+import Login from './COMPONENTS/LOGIN/Login.jsx'
+import Register from './COMPONENTS/REGISTER/Register.jsx';
+import {RouterProvider, createBrowserRouter} from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login></Login>
+  },
+  {
+    path: "/register",
+    element: <Register></Register>
+  },
+])
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router}/>
+  </React.StrictMode>
 )
